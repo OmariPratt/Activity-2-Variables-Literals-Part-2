0a010fb32c54a60f9310cd13afb31adb0fee6e77
Omari Pratt <omari.pratt@hgs.hiddengeniusproject.org>
2022-02-11 01:41:05 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var myname = "Omari";
+var mybros = "Sione & Peyton";
+var mymessage = "we will get better at coding";
+console.log(" My name is " + myname + ", I am on a team with " + mybros +". I want my bros to know that " +
+mymessage + ".");
